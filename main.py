4c3435280b63ef125cb1e2eb13cf40356376063f
shlomo-mica <shlomo.mica@outlook.co.il>
2023-06-16 00:51:35 +0300

    Update main.py

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-from flask import Flask, render_template, url_for
+ from flask import Flask, render_template, url_for, request
 import requests
+import smtplib
 
 blog_url = "https://startbootstrap.com/previews/clean-blog"
 clean_blog = requests.get(url=blog_url)
@@ -10,8 +11,9 @@ response = fetch_data.json()
 title = (response[0]['title'])
 subtitle = (response[0]['subtitle'])
 post_number = (response[1]['title'])
-body_detail=(response[2]['body'])
+body_detail = (response[2]['body'])
 print(body_detail)
+
 
 # print(response[0]['author'])
 # print(response[0]['dates'])
@@ -25,8 +27,8 @@ def hello_world():
 @app.route("/<int:post_id>")
 def posts(post_id):
     post_body = response[post_id]['body']
-    titlepost=response[post_id]['title']
-    return render_template("post.html", post=post_body,main_title=titlepost)
+    titlepost = response[post_id]['title']
+    return render_template("post.html", post=post_body, main_title=titlepost)
 
 
 @app.route("/about")
@@ -34,9 +36,52 @@ def about_page():
     return render_template("about.html")
 
 
+def email_send(name_details, mail):
+    my_email = "shlomo.course@gmail.com"
+    password = "@@@@@@@"
+    with smtplib.SMTP("smtp.gmail.com", 587) as connection:
+        connection.starttls()
+        connection.login(user=my_email, password=password)
+        connection.sendmail(from_addr=my_email, to_addrs="shlomo.course@gmail.com"
+                            , msg=f"new customer\n\n,{name_details}"
+                                  f" \n{mail}")
+
+
+def do_the_login():
+    return 'post'
+
+
+def show_the_login_form():
+    return 'get'
+
+
+@app.route('/login', methods=['GET', 'POST'])
+def login():
+    if request.method == 'POST':
+        return do_the_login()
+    else:
+        return email_send("eeee33333")
+
+
 @app.route("/contact")
 def contact_page():
     return render_template("contact.html")
+
+
+@app.route('/form_entry', methods=['GET', 'POST'])
+def receive_data():
+    data = request.form
+    var1 = 'SENDING...'
+    var2 = 'SUCCESS SENT YOR RESPONSE'
+    if request.method == "POST":
+        first_name = request.form.get("fname")
+        customer_mail = request.form.get("mail")
+        email_send(first_name, customer_mail)
+        print("first_name_____", first_name, customer_mail)
+        print(data['fname'],data['mail'])
+
+    return render_template("contact.html", va11=var1, va22=var2)
+    # '<h1 align="center" style="color:red;">SUCCESS SENT YOR RESPONSE<h1/>'
 
 
 if __name__ == "__main__":
